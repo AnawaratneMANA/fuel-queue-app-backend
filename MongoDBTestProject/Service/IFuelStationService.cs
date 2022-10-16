20d0267415690638f50efb3f9f401e6d4ff0ec79
AnawaratneMANA <it19086408@my.sliit.lk>
2022-10-16 15:55:56 +0530

    Add Fuel Station Service class Interface - UPDATE #1
    Retriving list class updated, Instruction comments added.

--- MongoDBTestProject/Service/IFuelStationService.cs
+++ MongoDBTestProject/Service/IFuelStationService.cs
@@ -5,9 +5,12 @@ namespace MongoDBTestProject.Service
     /* SERVICE CLASS - Fuel Stations */
     public interface IFuelStationService
     {
-        List<Student> GetFuelStations();
+        // List of fuel stations for the queue users references.
+        List<FuelStation> GetFuelStations();
+        // Single fuel station details for the Station Owner.
         FuelStation GetFuelStation(String id);
         FuelStation CreateFuelStation(FuelStation station);
+        // Update fuel station queue starting time and ending time.
         void UpdateFuelStation(String stationId, FuelStation station);
         void RemoveFuelStation(String id);
     }
