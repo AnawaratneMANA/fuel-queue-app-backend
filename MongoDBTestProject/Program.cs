d6ccc937e6ecfc1c2c6f2849d99ec145fd85930d
AnawaratneMANA <it19086408@my.sliit.lk>
2022-10-16 16:36:02 +0530

    Add new database connection to the config for testing
    This is a testing and temporary branch to test the new database connection to the MongoDB Atlas Database. Later to be merged to the "dev"

--- MongoDBTestProject/Program.cs
+++ MongoDBTestProject/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(builder.Configu
 builder.Services.AddScoped<IStudentService, StudentService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IAuthHashService, AuthHashService>();
+builder.Services.AddScoped<IFuelStationService, FuelStationService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ MongoDBTestProject/Service/FuelStationService.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Driver;
+using MongoDBTestProject.Model;
+
+namespace MongoDBTestProject.Service
+{
+    public class FuelStationService : IFuelStationService
+
+    {
+        private readonly IMongoCollection<FuelStation> _fuelStation;
+
+        // Init DB Connections
+        public FuelStationService(IStudentDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _fuelStation = database.GetCollection<FuelStation>(settings.FuelStationCollectionName);
+        }
+        // Create a new FuelStation
+        public FuelStation CreateFuelStation(FuelStation station)
+        {
+            _fuelStation.InsertOne(station);
+            return station;
+        }
+        // Get a single Fuel Station
+        public FuelStation GetFuelStation(string id)
+        {
+            throw new NotImplementedException();
+        }
+        // Get a List of Fuel Stations
+        public List<FuelStation> GetFuelStations()
+        {
+            throw new NotImplementedException();
+        }
+        // Remove a fuel station (APP-ADMIN)
+        public void RemoveFuelStation(string id)
+        {
+            throw new NotImplementedException();
+        }
+        // Update a existing Fuel Station
+        public void UpdateFuelStation(string stationId, FuelStation station)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
