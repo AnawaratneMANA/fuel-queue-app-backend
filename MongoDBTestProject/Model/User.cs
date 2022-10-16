2e21a79953caeea098bc6721fe81019be87773ac
AnawaratneMANA <it19086408@my.sliit.lk>
2022-10-16 15:26:06 +0530

    Update the User model class

--- MongoDBTestProject/Model/User.cs
+++ MongoDBTestProject/Model/User.cs
@@ -11,5 +11,14 @@ namespace MongoDBTestProject.Model
         public string Name { get; set; } = String.Empty;
         [BsonElement("age")]
         public string Age { get; set; } = String.Empty;
+        [BsonElement("usertype")]
+        public string UserType { get; set; } = String.Empty;
+        [BsonElement("hashedpassword")]
+        public string HashedPassword { get; set; } = String.Empty;
+        [BsonElement("email")]
+        public string Email { get; set; } = String.Empty;
+        [BsonElement("vehicletype")]
+        public string VehicleType { get; set; } = String.Empty;
+
     }
 }
